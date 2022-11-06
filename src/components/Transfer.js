a45440eea0838d7dd334fae9dcaf23ed65322179
umutyorulmaz <umutyorulmaz11@gmail.com>
2022-11-06 18:37:30 -0500

    save changes

--- src/components/Transfer.js
+++ src/components/Transfer.js
@@ -1,12 +1,53 @@
-import React from 'react';
+// tried to use labtest twitter container example to implement here
 
+import React, { useState, useEffect } from "react";
+import moment from "moment/moment";
 
-const Transfer =() => {
-    return (
-        <div>
-            <p>Transfer</p>
-        </div>
-    );
+const Transfer = ({ post }) => {
+  const [postInputField, setPostInputField] = useState("");
+  //const [commentsData, setCommentsData] = useState([]);
+
+  const handleTextChange = (event) => {
+    setPostInputField(event.target.value);
+    //console.log(postInputField);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!postInputField) return;
+
+    commentsData.push({ // I wanted change this part but not sure how to   
+      id: commentsData.length,
+      timestamp: moment().format("LTS"),
+      commentText: postInputField,
+      
+    });
+    setCommentsData(commentsData);
+    setPostInputField("");
+  };
+
+  const { label } = post;
+  //console.log(commentsData);
+  return (
+    <div className="transfer alert alert-primary">
+      <h2>{label}</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          required
+          placeholder="Enter Amount"
+          value={postInputField}
+          onChange={handleTextChange}
+        />
+        <span> </span>
+        <button className="btn btn-secondary" type="submit">
+          Submit
+        </button>
+      </form>
+ 
+    </div>
+  );
 };
-    
+
 export default Transfer;
